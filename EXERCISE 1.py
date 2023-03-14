766198fe88b323db845feb9e2475f4c23dc93176
nadyakartika <nadyakartika1608@student.uns.ac.id>
2023-03-14 13:55:24 +0700

    menyelesaikan tugas

---
+++ EXERCISE 1.py
@@ -0,0 +1,16 @@
+#input
+Nama = 'Nadya Kartika Aptarini'
+NIM = 'I0322091'
+Alamat = 'Solobaru, Sukoharjo, Jawa Tengah'
+Visualisasi = 'berada di sebuah perumahan yang tidak terlalu ramai'
+Harapan = 'bisa bekerja di perusahaan ternama'
+
+dict = {'a' : Nama, 'b' : NIM, 'c' : Alamat, 'd' : Visualisasi, 'e' : Harapan}
+
+
+#hasil
+print("Nama saya", dict['a'])
+print("NIM saya", dict['b'])
+print("Saya tinggal di", dict['c'])
+print("Visualisasi tempat tinggal saya yaitu", dict['d'])
+print("Harapan saya sebagai lulusan teknik industri adalah", dict['e'])
